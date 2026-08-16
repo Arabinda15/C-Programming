322a343641484c49bb0478361a0aa4160660ec98
Arabinda15 <arabindpradhan.12@gmail.com>
2026-08-16 23:49:56 +0530

    Add Keyword Program

---
+++ 01_basics/03_Keyword.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(){
+
+    //'int' is a keyword used to declare an integer variable
+    int age = 20;
+
+    //'float' is a keyword used to declare a floating-point variable
+    float height = 5.5;
+
+    //'char' is a keyword used to declare a character variable
+    char grade = 'A';
+
+    // 'if' and 'else' are keywords used for decision making
+    if (age>=18){
+        printf("You are an adult.\n");
+    } else {
+        printf("You are minor.\n");
+    }
+
+    // 'return' is a keyword used to return a value from a function
+    return 0;
+}
