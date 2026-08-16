eb628da6d3ae202806131090e50b1fec9f75b083
Arabinda15 <arabindpradhan.12@gmail.com>
2026-08-17 00:00:36 +0530

    Add identifier Program

---
+++ 01_basics/04_identifiers.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+//Function name as identifier
+void Display(){
+
+    // Variables name as identifiers
+    int age = 20;
+    float height = 5.5;
+    char grade = 'A';
+
+    printf("Age is %d\n", age);
+    printf("Age is %.1f\n", height);
+    printf("Age is %c\n", grade);
+
+
+
+}
+ int main(){
+ Display();
+ }
