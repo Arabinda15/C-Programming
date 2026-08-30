f68017b3aaa100c84e8a308de00c355af5acb8ca
Arabinda15 <arabindpradhan.12@gmail.com>
2026-08-30 09:32:39 +0530

    Add Arithmetic Operator Program

---
+++ 02_Operators/01_Arithmetic.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    int a = 10 ,b = 3 ;
+    printf("Arithmetic Operators in C\n");
+    printf("The value of a + b is %d\n ", a+b );
+    printf("The value of a - b is %d\n ", a-b );
+    printf("The value of a * b is %d\n ", a*b );
+    printf("The value of a / b is %d\n ", a/b );
+    printf("The value of a %% b is %d\n ", a%b );
+   
+    return 0;
+
+}
