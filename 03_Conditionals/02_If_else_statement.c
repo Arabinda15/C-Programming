db5bc7d919ac41333ea86ccc654540e37fb8453a
Arabinda15 <arabindpradhan.12@gmail.com>
2026-09-04 17:56:44 +0530

    Add if_else_statement Program

---
+++ 03_Conditionals/02_If_else_statement.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    int num ;
+    printf("Enter a number : ");
+    scanf("%d", &num);
+
+    if(num % 2 == 0){
+        printf("Entered number is an even number");
+    }else{
+        printf("Entered number is an odd number");
+    }
+    return 0;
+}
