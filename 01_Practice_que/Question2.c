e1b501536409ebef6eb19334a40fa4a8369a2c2a
Arabinda15 <arabindpradhan.12@gmail.com>
2026-09-04 10:07:00 +0530

    Edit 2nd Basics Practice Program

--- 01_Practice_que/Question2.c
+++ 01_Practice_que/Question2.c
@@ -1,12 +1,12 @@
-/*Write a C program to declare 
-two integer variables, assign values to them, 
-and print their sum, difference, and product.*/
+/*Write a C program to declare two integer
+variables, assign them values, and print
+their sum, difference, product, and quotient.*/
 
 #include<stdio.h>
 
 int main(){
     int a , b ;
-    int sum , difference , product ;
+    int sum , difference , product ,quotient;
 
     printf("Enter two numbers :");
     scanf("%d %d", &a ,&b);
@@ -14,6 +14,7 @@ int main(){
     printf("sum of numbers : %d\n", a+b);
     printf("difference of numbers : %d\n", a-b);
     printf("product of numbers : %d\n", a*b);
+    printf("Quotient of numbers :%d\n",a%b);
 
     return 0;
 }
