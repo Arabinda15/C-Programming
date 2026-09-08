ae648ef2c8b572804f51dec04455c71139099be1
Arabinda15 <arabindpradhan.12@gmail.com>
2026-09-08 19:39:47 +0530

    Add func_declare&define  Program

---
+++ 05_Functions/02_func_declare_define.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void greet(); //Function Declaration !
+
+int main(){
+    greet();
+    return 0;
+}
+
+void greet(){ //Function Definition
+    printf("Hello , Good Moring !");
+}
