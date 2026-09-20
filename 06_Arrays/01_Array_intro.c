3eb698de51ceee439a03396a5bf3f3f3e8a2ddf6
Arabinda15 <arabindpradhan.12@gmail.com>
2026-09-20 23:55:35 +0530

    Add 1st Array  program

---
+++ 06_Arrays/01_Array_intro.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    int arr[] = {10 , 20 , 30 , 40 , 50};
+    printf("the 1st index contain value : %d\n",arr[0]);
+    printf("the 2nd index contain value : %d\n",arr[1]);
+    printf("the 3rd index contain value : %d\n",arr[2]);
+    printf("the 4th index contain value : %d\n",arr[3]);
+    printf("the 5th index contain value : %d\n",arr[4]);
+    return 0;
+}
